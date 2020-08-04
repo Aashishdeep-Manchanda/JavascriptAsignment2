4678ae09c5357fdf2904eb8fe4c199f4218867a3
Aashishdeep-Manchanda <45440745+Aashishdeep-Manchanda@users.noreply.github.com>
2020-08-04 01:56:25 -0400

    user

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,24 @@
 'use strict';
 var express = require('express');
 var router = express.Router();
-
+var userModel = require('../models/user');
+/* GET users listing. */
 /* GET users listing. */
 router.get('/', function (req, res) {
-    res.send('respond with a resource');
+    try {
+        userModel.find({}, function (err, foundUsers) {
+            console.log(err);
+            console.log(foundUsers);
+            
+            res.render('users', { articles: foundUsers, user: req.user });
+        });
+    }
+    catch (err) {
+        console.log(err);
+    }
 });
+
+
+
 
 module.exports = router;
